abe9786abc3f1ede11dccdda7e88efe80d362eb8
Thisal Abeyrathna <thisal.abeyrathna@gmail.com>
2025-03-28 00:57:45 +0530

    remove main.js, target.offsettop change

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
             window.scrollTo({
-                top: target.offsetTop - 80,
+                top: target.offsetTop - 1,
                 behavior: 'smooth'
             });
         }
