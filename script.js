6223b5289b24f21cc2981a4126da622f5df42fcc
Thisal Abeyrathna <thisal.abeyrathna@gmail.com>
2025-03-28 01:16:17 +0530

    change padding-bottom in hero and about me sections

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
             window.scrollTo({
-                top: target.offsetTop - 1,
+                top: target.offsetTop - 0,
                 behavior: 'smooth'
             });
         }
